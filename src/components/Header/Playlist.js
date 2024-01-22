15e7344763f0e2cffe8a711470cc7f582a5dca33
fadelrazh <fadelrzsh@gmail.com>
2024-01-22 18:02:54 +0700

    first commit

---
+++ src/components/Header/Playlist.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Playlist() {
+  return (
+    <nav className="flex">
+      <ul>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Yippee!
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
